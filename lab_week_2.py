e6540ca7bd50cbb8e2f6d75afc49a5ccfdf196e5
MalaikaBatool123 <110975997+MalaikaBatool123@users.noreply.github.com>
2025-07-13 02:57:33 +0500

    week 1 and week 2 processing

--- lab_week_2.py
+++ lab_week_2.py
@@ -3,9 +3,153 @@ Python File that contains all the Tasks from Week 2 lab.
 Each task is labeled clearly and separated properly for better understanding.
 """
 
-# Task 1: Compare Temperatures
-print("="*40)
-print("Task 1: Compare Temperatures\n")
+
+print('\n\n\n')
+
+"""
+
+-------- Section 1: Comparisons and Conditionals --------
+
+"""
+
+"""Exercise 1"""
+# Comparison Operators
+
+# example 1
+is_true = False
+print("is_true:", is_true)
+
+# example 2
+is_true = 5 > 4
+print("5 > 4:", is_true)
+
+# example 3
+a = 5
+b = 10
+
+print(a == b)   # False
+print(a != b)   # True
+print(a <= b)   # True
+print(a >= b)   # False
+
+
+
+print('\n\n')
+
+"""Exercise 2"""
+# Logical Operators
+
+# example 1
+age = 25
+is_in_age_range = age > 20 and age < 30
+
+print("is_in_age_range:", is_in_age_range)
+
+# example 2
+x = 5
+y = 10
+
+print(x > 0 and y > 0)    # True
+print(x > 0 or y < 0)     # True
+print(not(x > 0))         # False
+
+
+print('\n\n')
+
+"""Exercise 3"""
+# If Conditionals
+# example 1
+age = 19
+age_group = "child"
+if age > 18:
+    age_group = "adult"
+    print(f"The age group is {age_group}")
+
+# example 2
+age = 13
+age_group = "child"
+if age > 18:
+    age_group = "adult"
+    print(f"The age group is {age_group}")
+
+
+print('\n\n')
+
+"""Exercise 4"""
+# If-else Conditionals
+
+# example 1
+wind_speed = 30
+if wind_speed < 10:
+    print("It is a calm day")
+else:
+    print("It is a windy day")
+    
+# example 2
+wind_speed = 5
+if wind_speed < 10:
+    print("It is a calm day")
+else:
+    print("It is a windy day")
+
+print('\n\n')
+
+
+"""Exercise 5"""
+# If-elif-else Conditionals
+
+# example 1
+grade = 55
+if grade < 50:
+    print("You failed")
+elif grade < 60:
+    print("You passed")
+elif grade < 70:
+    print("You got a good pass")
+else:
+    print("You got an excellent pass")
+
+# example 2
+grade = 40
+if grade < 50:
+    print("You failed")
+elif grade < 60:
+    print("You passed")
+elif grade < 70:
+    print("You got a good pass")
+else:
+    print("You got an excellent pass")
+
+# example 3
+grade = 65
+if grade < 50:
+    print("You failed")
+elif grade < 60:
+    print("You passed")
+elif grade < 70:
+    print("You got a good pass")
+else:
+    print("You got an excellent pass")
+
+# example 4
+grade = 80
+if grade < 50:
+    print("You failed")
+elif grade < 60:
+    print("You passed")
+elif grade < 70:
+    print("You got a good pass")
+else:
+    print("You got an excellent pass")
+    
+
+print('\n\n')
+
+
+"""Exercise 6"""
+
+# Task: Compare Temperatures
+print("Task: Compare Temperatures\n")
 
 # Just setting two temperature values
 temperature1 = 25
@@ -21,10 +165,96 @@ if temperature1 == temperature2:
 else:
     print("Result: The temperatures are different.")
 
-print("="*40)
+print('\n\n')
 
-# Task 2: Create, Access and Modify Lists
-print("Task 2: Create, Access and Modify Lists\n")
+
+""" 
+
+-------- Section 2: Python Lists --------
+
+"""
+
+"""Exercise 1"""
+# Creating a List
+integer_list = [1, 2, 3, 4, 5]
+
+# list of strings
+string_list = ["apple", "banana", "orange", "grape"]
+
+# empty list
+empty_list = []
+
+# list with different data types
+list_with_different_types = [1, "two", 3.0, True]
+
+person_1_age = 20
+person_2_age = 30
+# creating a list based on variables
+age_list = [person_1_age, person_2_age]
+
+# List within a List 
+list_within_a_list = [["red", "green", "blue"], ["yellow", "orange", "purple"]]
+
+# Task: Creating a List
+city_list = ["Glasgow", "London", "Edinburgh"]
+
+print('\n\n')
+
+"""Exercise 2"""
+# Accessing a List
+
+# using the string list from exercise 1
+
+# indices start from 0, so the second element is at index 1
+second_item = string_list[1]
+
+# Slicing
+
+# this slice from 0 to 2 gives the first two elements of index 0 and 1
+print(string_list[0:2])
+
+# using negative indices
+last_item = string_list[-1]  # last item is at index -1
+
+# Task
+
+# Third item
+print(city_list[2])       
+# Last two items using slicing
+print(city_list[1:])      
+
+
+print('\n\n')
+
+
+
+"""Exercise 3"""
+# Modifying a List
+
+# changing the first item (replacement)
+string_list[0] = "pear" 
+
+# adding an item to the end of the list
+string_list.append("orange") 
+
+
+
+# Task
+
+# Adding an item to list - at the end
+city_list.append("Manchester")
+
+# Modifying an item at index 1 - second element
+city_list[1] = "Birmingham"
+
+
+print('\n\n')
+
+
+"""Exercise 4"""
+
+# Task: Create, Access and Modify Lists
+print("Task: Create, Access and Modify Lists\n")
 
 # Creating a list of colours
 colours = ["blue", "green", "yellow"]
@@ -56,10 +286,77 @@ selected_colours = colours[1:3]
 # Showing the new small list
 print("Selected colours:", selected_colours)
 
-print("="*40)
+print('\n\n')
 
-# Task 3: Print Even Numbers from a List
-print("Task 3: Print Even Numbers from a List\n")
+
+""" 
+-------- Section 3: Python Loops --------
+
+"""
+
+
+"""Exercise 1"""
+# While Loop
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+print('\n\n')
+
+
+"""Exercise 2"""
+# For Loop
+for fruit in string_list:
+    print(fruit)
+
+print('\n\n')
+
+
+# Task
+# loop for city list
+for city in city_list:
+    print(city)
+
+print('\n\n')
+
+
+"""Exercise 3"""
+# Loop Keywords: Range, break and continue
+
+# range()
+# function to generate a series of numbers to use in a for loop
+# or want to perform a task a certain number of times
+for number in range(1, 6):
+    print(number)
+    
+# break
+# it will stop the loop from running any more iterations
+for i in range(5):
+    if i == 2:
+        break   
+    print(i)
+
+# Task: print the numbers 0 through 4, but stop the loop when i is equal to 3
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+# Continue
+# it will skip the current iteration and continue to the next iteration
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+    
+print('\n\n')
+
+
+"""Exercise 4"""
+
+# Task: Print Even Numbers from a List
+print("Task: Print Even Numbers from a List\n")
 
 # A simple list of numbers from 1 to 10
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -70,10 +367,10 @@ for num in numbers:
     if num % 2 == 0:
         print(num)
 
-print("="*40)
+print('\n\n')
 
-# Task 4: Sum of Squares
-print("Task 4: Sum of Squares\n")
+# Task: Sum of Squares
+print("Task: Sum of Squares\n")
 
 # Starting the total from 0
 sum_of_squares = 0
@@ -85,10 +382,10 @@ for number in range(1, 6):
 # Showing the final result
 print("The sum of squares from 1 to 5 is:", sum_of_squares)
 
-print("="*40)
+print('\n\n')
 
-# Task 5: Countdown from 10 to 1 using while loop
-print("Task 5: Countdown from 10 to 1 using while loop\n")
+# Task: Countdown from 10 to 1 using while loop
+print("Task: Countdown from 10 to 1 using while loop\n")
 
 # Start countdown at 10
 countdown = 10
@@ -101,10 +398,31 @@ while countdown >= 1:
 # Once done, show this message
 print("Liftoff!")
 
-print("="*40)
+print('\n\n')
 
-# Task 6: Take age input from the user and give a message
-print("Task 6: Take age input from the user and give a message\n")
+"""
+
+-------- Section 4: Obtaining User Input --------
+
+"""
+
+# input()
+# function is used to take input from the user
+user_input = input("Enter something: ")
+print("You entered:", user_input)
+
+
+# Example - entering a number
+age = input("How old are you? ")
+# Convert the age to an integer
+age = int(age)
+next_year_age = age + 1
+print("Next year, you'll be", next_year_age, "years old.")
+
+print('\n\n')
+
+# Task: User Input and Conditionals
+print("Task: User Input and Conditionals\n")
 
 # Ask the user to enter their age
 age = int(input("Enter your age: "))
@@ -117,10 +435,11 @@ elif age <= 65:
 else:
     print("You are a senior citizen.")
 
-print("="*40)
 
-# Task 7: Temperature Converter with user input and options
-print("Task 7: Temperature Converter with user input and options\n")
+print('\n\n')
+
+# Task: Temperature Converter with user input and options
+print("Task: Temperature Converter with user input and options\n")
 
 while True:
     # Ask the user what type of temperature they want to convert
--- lab_week_3.py
+++ lab_week_3.py
@@ -3,11 +3,52 @@ Python File that contains all the Tasks from Week 3 lab.
 Each task is labeled clearly and separated properly for better understanding.
 """
 
+"""
+-------- Section 1: Functions and Scope --------"""
 
-# Task 1: Greet each friend in the list
 
-print("="*40)
-print("Task 1: Greet each friend in the list\n")
+"""Exercise 1"""
+# Functions in Python
+# A function is a block of code which only runs when it is called
+
+# creating function
+def greet_user():
+    print("Hello!")
+# calling function
+greet_user()
+
+# function with parameters
+def greet_user(name):
+    print(f"Hello {name}!")
+
+# calling function
+greet_user("John")
+
+# functions with more than one parameter
+def greet_user(first_name, last_name):
+    print(f"Hello {first_name} {last_name}!")
+
+# calling function
+greet_user("John", "Doe")
+
+# keyword arguments
+greet_user(last_name="Smith", first_name="John")
+
+
+# Default values
+def greet_user(first_name, last_name, university="UWS"):
+    print(f"Hello {first_name} {last_name} from {university}!")
+
+# calling function
+greet_user("John", "Doe")
+# it will also work when we pass the value of variable which have default value
+greet_user("John", "Smith", "UWS London")
+
+print('\n\n')
+
+
+# Task: Greet each friend in the list
+print("Task: Greet each friend in the list\n")
 
 def greet_friends(friend_list):
     for name in friend_list:
@@ -19,11 +60,44 @@ friends = ["John", "Jane", "Jack"]
 # Calling the function
 greet_friends(friends)
 
-print("="*40)
+print('\n\n')
 
 
-# Task 2: Calculate Tax Based on Income and Tax Rate
-print("Task 2: Calculate Tax Based on Income and Tax Rate\n")
+
+# Returning values from functions
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# calling function
+result = add_numbers(5, 3)
+print(f"The sum of 5 and 3 is: {result}")
+
+print('\n\n')
+
+# returning multiple values
+def add_and_multiply_numbers(num1, num2):
+    return num1 + num2, num1 * num2
+
+def add_and_multiply_numbers(num1, num2):
+    sum = num1 + num2
+    product = num1 * num2
+    return sum, product
+
+# calling function and getting multiple values
+result_sum, result_product = add_and_multiply_numbers(5, 3)
+print(f"The sum of 5 and 3 is: {result_sum}")
+print(f"The product of 5 and 3 is: {result_product}")
+
+print('\n\n')
+
+
+
+# Task: Calculate Tax Based on Income and Tax Rate
+print("Task: Calculate Tax Based on Income and Tax Rate\n")
 
 # Step 1: Define the tax calculation function
 def calculate_tax(income, tax_rate):
@@ -44,6 +118,7 @@ while True:
         income = float(input("Enter the income in £: "))
         tax_rate = float(input("Enter the tax rate (e.g. 0.2 for 20%): "))
 
+        # Step 4: Call the tax calculation function
         # Calculate and print the tax
         tax = calculate_tax(income, tax_rate)
         print(f"The tax on £{income} at a {tax_rate * 100}% rate is: £{tax}")
@@ -57,14 +132,15 @@ while True:
         print("Invalid input. Please type 'y' or 'n'.")
         
         
-print("="*40)
+print('\n\n')
 
 
 
 
-# Task 3: Compound Interest Calculator Function
-print("Task 3: Compound Interest Calculator Function\n")
+# Task: Compound Interest Calculator Function
+print("Task: Compound Interest Calculator Function\n")
 
+# function have three parameters
 def compound_interest(principal, duration, interest_rate):
     # Check if interest_rate is valid
     if interest_rate < 0 or interest_rate > 1:
@@ -88,58 +164,107 @@ def compound_interest(principal, duration, interest_rate):
 # Example test
 final_result = compound_interest(1000, 5, 0.03)
 print(f"\nFinal investment value after 5 years: {final_result:.2f} £")
-# assert compound_interest(1000, 5, 0.03) == 1159
+assert compound_interest(1000, 5, 0.03) == 1159
 
 
 
-print("="*40)
+print('\n\n')
 
 
-# Task 4: Variable Scope
+"""Exercise 2"""
+# Variable Scope
+
+def new_function():
+    my_new_variable = 5
+
+new_function() # call the function. No problems here.
+"""
+this will cause an error because this variable is 
+defined inside the function and can only be 
+accessed and used inside the function
+"""
+
+# print(my_new_variable) 
+
+print('\n\n')
+
+"""variables defined outside the function 
+can be accessed inside the function too """
+
+
+
+""" 
+-------- Section 2: Assertions and Errors --------
+"""
+
+"""Exercise 6"""
+# Assertions
+# Example test for compound interest function that was in section 1 Task
+assert compound_interest(1000, 5, 0.03) == 1159 
+
+print('\n\n')
+
+"""Exercise 7"""
+# Identifying and Fixing Common Errors 
 
 # Task 5: Fixing Errors
 print("Task 5: Fixing Errors\n")
 
 
 # Syntax Error
-print("Syntax Error")
-print("Wrong Code: print('Hello, World!')")
-# pritn("Hello, World!")  
+pritn("Hello, World!")  
 
 # Corrected Code
-print("Correct Code")
-print("print('Hello, World!')")
+print("Hello, World!")
 
-print("-"*40)
+print('\n\n')
+
 
 # Name Error: 
-print("Name Error")
-print("Wrong Code: print('My favorite color is', favorite_color)")
-# print("My favorite color is", favorite_color) 
+my_name = "Alice" 
+print("Hello, " + myname) 
 
 # Corrected Code:
+# define the variable correctly and then using it
+favorite_color = "Blue"  
+print("My favorite color is", favorite_color)
 
-# Define the variable before using it
-print('Define the Variable before using it \nfavorite_color = "Blue" \nprint("My favorite color is", favorite_color)')
-
-# favorite_color = "Blue"  
-# print("My favorite color is", favorite_color)
-
+print('\n\n')
 
 
 # Value Error: 
-# number1 = "5" 
-# number2 = 3 
-# result = number1 + number2 
+number1 = "5" 
+number2 = 3 
+result = number1 + number2 
 
 # Corrected Code:
-# Convert the string to an integer
+# Convert the string to an integer then perform the operation
 number1 = int("5")  
 number2 = 3  
 result = number1 + number2
 
 
 
+print('\n\n')
+
+# Index Error
+fruits = ["apple", "banana", "orange"] 
+print(fruits[3])
+
+# Corrected Code:
+# Use a valid index for the list
+# Index starts from 0, thats why end at 1 less then the length of the list
+fruits = ["apple", "banana", "orange"] 
+print(fruits[2])
 
 
+
+# Indentation Error
+if 5 > 2: 
+print("Five is greater than two!") 
+
+# Corrected Code:
+# Indentation is correct
+if 5 > 2:
+    print("Five is greater than two!")
 
